2f143981a72622019fe69dae2fa88640add14ea2
jacky <jacky.yuan@dcmail.ca>
2020-10-01 21:54:59 -0400

    XAML lab 1: Code It Inc (form + validations created)

---
+++ Net3202_Lab1_CodeItInc/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Net3202_Lab1_CodeItInc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCreateProject_Click(object sender, RoutedEventArgs e)
+        {
+            string inputtedProjectName;
+            double inputtedProjectSpent;
+            double inputtedProjectBudget;
+            double inputtedHoursRemaining;
+            int inputtedProjectStatus;
+
+            if (txtProjectName.Text.Trim() != string.Empty)
+            {
+                inputtedProjectName = txtProjectName.Text.Trim();
+                if (Double.TryParse(txtBudget.Text.Trim(), out inputtedProjectBudget))
+                {
+                    if (inputtedProjectBudget >= 0)
+                    {
+                        if (Double.TryParse(txtSpent.Text.Trim(), out inputtedProjectSpent))
+                        {
+                            if (inputtedProjectSpent >= 0)
+                            {
+                                if (Double.TryParse(txtEstHoursRemaining.Text.Trim(), out inputtedHoursRemaining))
+                                {
+                                    if (inputtedHoursRemaining >= 0)
+                                    {
+                                        inputtedProjectStatus = cmbRequirements.SelectedIndex;
+                                    }
+                                    else if (inputtedHoursRemaining == 0)
+                                    {
+                                        cmbRequirements.SelectedIndex = 5;
+                                        inputtedProjectStatus = cmbRequirements.SelectedIndex;
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Error: Cannot have negative hours remaining."); 
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Error: Estimated time must be a numeric input.");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error: Cannot amount spent cannot be negative value.");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error: Amount Spent must be a numeric input.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error: Cannot budget cannot be negative value.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error: Budget must be a numeric input.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: Project name cannot be empty.");
+            }
+        }
+    }
+}
---
+++ Net3202_Lab1_CodeItInc/Program.cs
@@ -0,0 +1,54 @@
+﻿//These are default (keep them!)
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+//keep the default namespace too, BillingApp is just the name of my project.
+namespace Net3202_Lab1_CodeItInc
+{
+    public class Program
+    {
+        //First private data member
+        private string projectName;
+        private double projectBudget;
+        private double projectSpent;
+        private double hoursRemaining;
+        private string projectStatus;
+
+        //Complete the rest (there are four more mentioned in the description).
+        //constructor, you need to complete it this is a no parameter constructor
+       // shown below.
+    public Program(string projectName, double projectBudget, double projectSpent, double hoursRemaining, string projectStatus)
+        {
+
+        }
+            
+        public string ProjectName
+        {
+            get { return this.projectName; }
+            set { this.projectName = value; }
+        }
+    public double ProjectBudget
+        { 
+            get { return this.projectBudget; }
+            set { this.projectBudget = value; }
+        }
+    public double ProjectSpent
+        {
+            get { return this.projectSpent; }
+            set { this.projectSpent = value; }
+        }
+    public double HoursRemaining
+        {
+            get { return this.hoursRemaining; }
+            set { this.hoursRemaining = value; }
+        }
+    public string ProjectStatus
+        {
+            get { return this.projectStatus; }
+            set { this.projectStatus = value; }
+        }
+
+    }
+   
+}
